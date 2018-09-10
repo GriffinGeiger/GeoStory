aca4c0534069dd6e5bacac64fa8e7aa7e9e2307f
GriffinGeiger <griffin2420@yahoo.com>
2018-09-10 10:18:21 -0400

    Created multi-touch scroll rect to attempt making the pinch zoom less jumpy

---
+++ Assets/Scripts/MultiTouchScrollRect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MultiTouchScrollRect : ScrollRect, IDragHandler
+{
+    private bool multitouchDragging;
+
+    public override void OnDrag(PointerEventData eventData)
+    {
+        if(Input.touchCount ==1)
+        {
+            base.OnDrag(eventData);
+        }
+        else if(Input.touchCount> 1)
+        {
+            eventData.pressPosition = 
+                Vector3.Lerp(Input.GetTouch(0).position, Input.GetTouch(1).position, .5f);
+            base.OnDrag(eventData);
+        }
+    }
+    
+}
